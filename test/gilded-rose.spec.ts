11d2d637fe6e25199fd483c1cb1fea58c2336b66
Balint Peak <balint.peak@gmail.com>
2019-04-07 15:04:02 +0100

    move item names to variable in tests

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -12,10 +12,12 @@ describe('Gilded Rose', function (): void {
     })
 
     describe('Generic item', function (): void {
+        const itemName = 'generic item'
+
         it('should decrease sellin by one', function (): void {
             const gildedRose = new GildedRose([
-                new Item('generic item', 1, 20),
-                new Item('generic item', 0, 20),
+                new Item(itemName, 1, 20),
+                new Item(itemName, 0, 20),
             ])
             const updatedItems = gildedRose.updateQuality()
 
@@ -24,7 +26,7 @@ describe('Gilded Rose', function (): void {
         })
 
         it('should decrease quality by one, when the sell by date has not yet passed', function (): void {
-            const gildedRose = new GildedRose([new Item('generic item', 2, 20)])
+            const gildedRose = new GildedRose([new Item(itemName, 2, 20)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(19)
@@ -32,8 +34,8 @@ describe('Gilded Rose', function (): void {
 
         it('should decrease quality twice as fast, once the sell by date has passed', function (): void {
             const gildedRose = new GildedRose([
-                new Item('generic item', 0, 20),
-                new Item('generic item', -1, 20),
+                new Item(itemName, 0, 20),
+                new Item(itemName, -1, 20),
             ])
             const updatedItems = gildedRose.updateQuality()
 
@@ -42,7 +44,7 @@ describe('Gilded Rose', function (): void {
         })
 
         it('should never have an item with negative quality', function (): void {
-            const gildedRose = new GildedRose([new Item('generic item', 10, 0)])
+            const gildedRose = new GildedRose([new Item(itemName, 10, 0)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(0)
@@ -50,15 +52,16 @@ describe('Gilded Rose', function (): void {
     })
 
     describe('Aged Brie', function (): void {
+        const itemName = 'Aged Brie'
         it('should increase quality', function (): void {
-            const gildedRose = new GildedRose([new Item('Aged Brie', 10, 20)])
+            const gildedRose = new GildedRose([new Item(itemName, 10, 20)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(21)
         })
 
         it('should not have quality above 50', function (): void {
-            const gildedRose = new GildedRose([new Item('Aged Brie', 10, 50)])
+            const gildedRose = new GildedRose([new Item(itemName, 10, 50)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(50)
@@ -66,22 +69,23 @@ describe('Gilded Rose', function (): void {
     })
 
     describe('Sulfuras', function (): void {
+        const itemName = 'Sulfuras, Hand of Ragnaros'
         it('should not change quality when sellin is positive', function (): void {
-            const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 10, 80)])
+            const gildedRose = new GildedRose([new Item(itemName, 10, 80)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(80)
         })
 
         it('should not change quality when sellin is zero', function (): void {
-            const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', 0, 80)])
+            const gildedRose = new GildedRose([new Item(itemName, 0, 80)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(80)
         })
 
         it('should not change quality when sellin is negative', function (): void {
-            const gildedRose = new GildedRose([new Item('Sulfuras, Hand of Ragnaros', -1, 80)])
+            const gildedRose = new GildedRose([new Item(itemName, -1, 80)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(80)
@@ -90,7 +94,7 @@ describe('Gilded Rose', function (): void {
 
     describe('Backstage passes', function (): void {
         it('should increase quality by 1 when sellin is bigger than 10', function (): void {
-            const gildedRose = new GildedRose([new Item('Backstage passes to a TAFKAL80ETC concert', 11, 20)])
+            const gildedRose = new GildedRose([new Item(itemName, 11, 20)])
             const updatedItems = gildedRose.updateQuality()
 
             expect(updatedItems[0].quality).to.equal(21)
@@ -98,9 +102,9 @@ describe('Gilded Rose', function (): void {
 
         it('should increase quality by 2 when 10 >= sellin > 5', function (): void {
             const gildedRose = new GildedRose([
-                new Item('Backstage passes to a TAFKAL80ETC concert', 10, 20),
-                new Item('Backstage passes to a TAFKAL80ETC concert', 6, 20),
-                new Item('Backstage passes to a TAFKAL80ETC concert', 6, 50),
+                new Item(itemName, 10, 20),
+                new Item(itemName, 6, 20),
+                new Item(itemName, 6, 50),
             ])
             const updatedItems = gildedRose.updateQuality()
 
@@ -111,9 +115,9 @@ describe('Gilded Rose', function (): void {
 
         it('should increase quality by 3 when 0 < sellin <= 5', function (): void {
             const gildedRose = new GildedRose([
-                new Item('Backstage passes to a TAFKAL80ETC concert', 5, 20),
-                new Item('Backstage passes to a TAFKAL80ETC concert', 1, 20),
-                new Item('Backstage passes to a TAFKAL80ETC concert', 1, 50),
+                new Item(itemName, 5, 20),
+                new Item(itemName, 1, 20),
+                new Item(itemName, 1, 50),
             ])
             const updatedItems = gildedRose.updateQuality()
 
@@ -124,8 +128,8 @@ describe('Gilded Rose', function (): void {
 
         it('should set quality to 0 when sellin <= 0', function (): void {
             const gildedRose = new GildedRose([
-                new Item('Backstage passes to a TAFKAL80ETC concert', 0, 20),
-                new Item('Backstage passes to a TAFKAL80ETC concert', -1, 20),
+                new Item(itemName, 0, 20),
+                new Item(itemName, -1, 20),
             ])
             const updatedItems = gildedRose.updateQuality()
 
